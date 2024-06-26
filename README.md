71531a030e90fbe1c21fbf2c38671dfec2f0eb24
Simon Rojas <sdavid.rojas@hotmail.com>
2024-06-26 12:53:48 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Backend en Nest
+```
+docker compose up -dd
+```
+
+jasodjoasjdoas
+
+Copiar el ```.env.template``` y renombrarlo a ```.env```
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,65 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto, UpdateAuthDto, LoginDto, RegisterUserDto } from './dto';
+import { AuthGuard } from './guards/auth.guard';
+import { User } from './entities/user.entity';
+import { LoginResponse } from './interfaces/login-response';
+
+
+
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.authService.create(createUserDto);
+  }
+
+  @Post('/login')
+  login( @Body() LoginDto: LoginDto ) {
+    return this.authService.login( LoginDto )
+  }
+
+  @Post('/register')
+  register( @Body() registerDto: RegisterUserDto ) {
+    return this.authService.register( registerDto )
+  }
+
+  @UseGuards( AuthGuard )
+  @Get()
+  findAll( @Request() req: Request ) {
+
+    const user = req['user'];
+    return user;
+  }
+
+  //LoginResponse
+  @UseGuards( AuthGuard )
+  @Get('check-token')
+  checkToken( @Request() req: Request ): LoginResponse{
+    const user = req['user'] as User;
+
+    return {
+      user: user,
+      token: this.authService.getJwToken( { id: user._id } )
+    }
+  }
+
+  /*
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.authService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+    return this.authService.update(+id, updateAuthDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.authService.remove(+id);
+  }*/
+}
